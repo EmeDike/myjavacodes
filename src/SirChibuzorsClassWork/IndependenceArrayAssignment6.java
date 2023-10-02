135dd00b859c0a3d83aef400b37ddc112f7dfaa9
Eme Dike <dicksoneme64@gmail.com>
2023-10-02 01:58:36 +0100

    my java code update

--- src/SirChibuzorsClassWork/IndependenceArrayAssignment6.java
+++ src/SirChibuzorsClassWork/IndependenceArrayAssignment6.java
@@ -1,4 +1,17 @@
 package SirChibuzorsClassWork;
 
 public class IndependenceArrayAssignment6 {
+    public static int [] runningTotal(int [] arr) {
+        int length = arr.length;
+        int []result = new int[length];
+        int sum = 0;
+        int runningTotal;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            result[i] = sum;
+        }
+        return result;
+
+
+    }
 }
