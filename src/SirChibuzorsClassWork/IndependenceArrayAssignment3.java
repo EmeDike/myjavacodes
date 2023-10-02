b73e6a9edffd2fa92ccab4c5288775fd8d67096c
Eme Dike <dicksoneme64@gmail.com>
2023-10-02 01:28:59 +0100

    my java code update

--- src/SirChibuzorsClassWork/IndependenceArrayAssignment3.java
+++ src/SirChibuzorsClassWork/IndependenceArrayAssignment3.java
@@ -1,4 +1,16 @@
 package SirChibuzorsClassWork;
 
 public class IndependenceArrayAssignment3 {
-}
+    // A function that checks whether an element occurs in a list.
+    public static boolean checkElement(int []arr, int element){
+
+        for(int number : arr) {
+            if (number == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    }
--- src/SirChibuzorsClassWork/IndependenceArrayAssignment4.java
+++ src/SirChibuzorsClassWork/IndependenceArrayAssignment4.java
@@ -1,4 +1,13 @@
 package SirChibuzorsClassWork;
 
 public class IndependenceArrayAssignment4 {
+    public static int[] oddPositions(int[] arr) {
+        int oddPositionLength = (arr.length + 1)/2;
+        int [] result = new int[oddPositionLength];
+        for (int i = 0; i < arr.length; i ++) {
+            result[i] = arr[i * 2 + 1];
+        }
+            return result;
+    }
+
 }
