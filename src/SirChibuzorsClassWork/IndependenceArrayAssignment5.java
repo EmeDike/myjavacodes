703b7e71801af9e9f2fd36020495d2fc1ce9815b
Eme Dike <dicksoneme64@gmail.com>
2023-10-02 01:33:49 +0100

    My java code update

--- src/SirChibuzorsClassWork/IndependenceArrayAssignment5.java
+++ src/SirChibuzorsClassWork/IndependenceArrayAssignment5.java
@@ -1,4 +1,12 @@
 package SirChibuzorsClassWork;
-
 public class IndependenceArrayAssignment5 {
-}
+        public static int[] evenPositions(int[] arr) {
+            int evenPositionLength = (arr.length )/2;
+            int [] result = new int[evenPositionLength];
+            for (int i = 0; i < arr.length; i ++) {
+                result[i] = arr[i * 2];
+            }
+            return result;
+        }
+
+    }
